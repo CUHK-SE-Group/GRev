1f4ccf051f42622d8c0ce9f7c7a3f46dc099e1dc
lincyawer <53161583+Lincyaw@users.noreply.github.com>
2023-04-20 12:16:07 +0800

    添加请求neo4j示例

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import logging
 
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler()
+file_handler = logging.FileHandler('log_file.log')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 console_handler.setFormatter(formatter)
 console_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.INFO)
 logger.addHandler(console_handler)
+logger.addHandler(file_handler)
--- gdb_factory.py
+++ gdb_factory.py
@@ -1,5 +1,9 @@
+import logging
+
 from neo4j import GraphDatabase, basic_auth
 from abc import ABC, abstractmethod
+
+from neo4j.exceptions import Neo4jError
 
 
 class GdbFactory(ABC):
@@ -8,16 +12,28 @@ class GdbFactory(ABC):
         pass
 
 
-class Neo4j(GdbFactory):
+class Neo4j:
     def __init__(self, uri, username, passwd):
         self.driver = GraphDatabase.driver(uri, auth=basic_auth(username, passwd))
         self.session = self.driver.session()
 
     def run(self, query):
-        self.session.run(query)
+        return self.session.run(query)
 
     def __del__(self):
         self.session.close()
         self.driver.close()
 
 
+if __name__ == "__main__":
+    client = Neo4j("bolt://10.20.10.27:7687", "neo4j", "testtest")
+    with open("query_file/query.log", 'r') as f:
+        query = f.readline()
+        while query != '':
+            query.replace('\n', '')
+            try:
+                result = client.run(query)
+                print(len(list(result)))
+            except Neo4jError as e:
+                logging.error("An error occurred: ", e)
+            query = f.readline()
