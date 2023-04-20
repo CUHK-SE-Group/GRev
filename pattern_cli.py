458118bca916e73eeb4a7ad381406230d2f8771b
Joyemang <joyemang33@gmail.com>
2023-04-20 15:29:37 +0800

    修改了query/create.log，修复率PatternTransformer孤立点的Bug，增加了test cases

---
+++ pattern_cli.py
@@ -0,0 +1,17 @@
+import click
+from pattern_transformer import *
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.argument('pattern')
+def trans(pattern: str):
+    p = PatternTransformer()
+    asg = p.pattern2asg(pattern)
+    pattern1 = p.asg2pattern(asg)
+    click.echo(pattern1)
+
+if __name__ == '__main__':
+    cli()
