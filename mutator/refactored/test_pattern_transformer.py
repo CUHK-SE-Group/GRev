925e8f004f0dc0dc8e1aff7a11c29e51784a971f
polyominal <79344013+polyominal@users.noreply.github.com>
2023-06-26 00:07:44 +0800

    [mutator] Node&path patterns tests

--- mutator/refactored/test_pattern_transformer.py
+++ mutator/refactored/test_pattern_transformer.py
@@ -8,4 +8,4 @@ def test_parse_node_pattern():
 def test_parse_path_pattern():
     pt = PatternTransformer()
     print(pt.parse_path_pattern("(n:Person)-[r:FRIEND]->(m:Person)<-[s:ENEMY]-(o:Person)"))
-    print(pt.parse_path_pattern("(n9:(((L5)|L6|!!L5|!!(L1))) { p: 10, q: date('2023-02-10') })-[]-(n:HUMAN&DICK)"))
+    print(pt.parse_path_pattern("(n9:(((L5)|L6|!!L5|!!(L1))) { p: 10, q: date('2023-02-10') })-[]-(bob:(L7&L5))"))
