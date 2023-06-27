0af5e852f6f309378aae8ad25dad5f7b2a597928
joyemang33 <joyemang33@gmail.com>
2023-06-27 15:12:44 +0800

    Modify Generator

--- cypher/query_generator.py
+++ cypher/query_generator.py
@@ -1,4 +1,12 @@
 # The main file that produce the query statements
+import random
+from cypher.pattern_clause import PatternGenerator
+from cypher.schema import GraphSchema
+
+class QueryGenerator:
+    def __init__(self, G : GraphSchema):
+        self.pattern_generator = PatternGenerator(self.G)
+
 
 
 if __name__ == "__main__":
