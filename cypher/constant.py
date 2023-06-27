ff2170c06adb0dd8ae1454d0c1ad588dcd387992
lincyawer <53161583+Lincyaw@users.noreply.github.com>
2023-06-27 11:17:50 +0800

    update naming convention

--- cypher/constant.py
+++ cypher/constant.py
@@ -1,8 +1,9 @@
 import string
 import random
 
+
 class ConstantGenerator:
-    
+
     def gen(self, mytype):
         if mytype == "int":
             value = random.randint(-(2 ** 63), (2 ** 63) - 1)
@@ -10,13 +11,15 @@ class ConstantGenerator:
         if mytype == "float":
             value = random.random() * random.randint(-(2 ** 63), (2 ** 63) - 1)
             return format(value, '.20f')
-            
+
         if mytype == "bool":
-            if random.randint(0, 1) == 1: return "true"
-            else: return "false"
-        
+            if random.randint(0, 1) == 1:
+                return "true"
+            else:
+                return "false"
+
         if mytype == "string":
             length = random.randint(1, 20)
-            ran = ''.join(random.choices( \
-                string.ascii_uppercase + string.ascii_lowercase + string.digits, k = length))
+            ran = ''.join(random.choices(
+                string.ascii_uppercase + string.ascii_lowercase + string.digits, k=length))
             return '"' + ran + '"'
